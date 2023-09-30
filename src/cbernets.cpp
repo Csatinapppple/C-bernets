ec18dfb0faf97688e54cfaf451e2851b58e6f08d
Csatinapppple <88691457+Csatinapppple@users.noreply.github.com>
2023-09-30 20:22:49 -0300

    Add files via upload

--- src/cbernets.cpp
+++ src/cbernets.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <cstdio>
 #include <unistd.h>
@@ -7,9 +6,7 @@
 #include <cctype>
 #include <climits>
 
-#define DEBUG 0
-
-typedef unsigned int uint;
+#define DEBUG 1
 
 enum Status
 {
@@ -18,29 +15,34 @@ enum Status
   DONE = 2,
 };
 
-class Job;
+class Pod;
 struct Task
 {
   int ram, cpu, status, i;
-  Job *current_job;
+  Pod *this_pod;
 };
 
-class Job
+class Pod
 {
 public:
   Task * tasks;
-  int size, done;
+  volatile int size, done;
   pthread_mutex_t mutex;
-  Job (int size)
+  
+  Pod (int size)
   {
     this->size = size;
+    printf("pod size = %d\n",this->size);
+
     tasks = (Task *) calloc (size, sizeof (Task));
+    
     for (int i = 0; i < size; i++)
     {
-      (tasks + i)->current_job = this;
+      (tasks + i)->this_pod = this;
       (tasks + i)->ram = random () % 1000 + 500;
       (tasks + i)->cpu = random () % 4 + 1;
 #if DEBUG == 1
+      printf("this[%d] = %p\n", i, this);
       printf ("ram[%d] = %d\n", i, (tasks + i)->ram);
       printf ("cpu[%d] = %d\n", i, (tasks + i)->cpu);
 #endif
@@ -48,6 +50,7 @@ public:
       (tasks + i)->i = i;
     }
     mutex = PTHREAD_MUTEX_INITIALIZER;
+  
   }
 
   Task *get_smallest_task_ram ()
@@ -109,23 +112,23 @@ public:
 
 };
 
-class Pod;
+class Worker;
 struct Pkg
 {
   Task *t;
-  Pod *current_pod;
+  Worker *current_worker;
 };
 
 void *work (void *ptr);
 
-class Pod
+class Worker
 {
 public:
   int ram, cpu;
 
   pthread_mutex_t mutex;
 
-  Pod (int ram, int cpu)
+  Worker (int ram, int cpu)
   {
     this->ram = ram, this->cpu = cpu;
     mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -134,17 +137,24 @@ public:
   void add_worker (Task * t)
   {
     pthread_t pt;
-    if ((this->ram - t->ram) < 0 || (this->cpu - t->cpu) < 0)
+
+    if ((this->ram - t->ram) <= 0 || (this->cpu - t->cpu) <= 0)
     {
       printf ("ERROR ADDING WORKER: NOT ENOUGH RAM(%d) OR MEMORY(%d)\n",
         this->ram - ram, this->cpu - cpu);
       return;
     }
+
+    t->status = IN_PROGRESS;
     this->ram -= t->ram;
     this->cpu -= t->cpu;
+    
     Pkg *p = (Pkg *) malloc (sizeof (Pkg));
+    
     p->t = t;
-    p->current_pod = this;
+    p->current_worker = this;
+    
+
     pthread_create (&pt, NULL, &work, p);
     pthread_detach (pt);
   }
@@ -154,73 +164,123 @@ void *
 work (void *ptr)
 {
   Pkg p = *(Pkg *) ptr;
+  
+  printf("p.Task = %p, p.current_worker = %p\n",p.t->this_pod, p.current_worker);
+  printf("p.Task.size = %p\n",&p.t->this_pod->size);
+  
   free (ptr);
-  sleep (random () % 10);
-  pthread_mutex_lock (&p.current_pod->mutex);
-  //access memory in Pod
+
+  sleep (random () % 10 + 1);
+  pthread_mutex_lock (&p.current_worker->mutex);
+  
+  //access memory in Worker
   printf ("workdone, returning %d ram and %d cpu\n", p.t->ram, p.t->cpu);
-  p.current_pod->ram += p.t->ram;
-  p.current_pod->cpu += p.t->ram;
-  pthread_mutex_unlock (&p.current_pod->mutex);
+  
+  p.current_worker->ram += p.t->ram;
+  p.current_worker->cpu += p.t->cpu;
+  
+  pthread_mutex_unlock (&p.current_worker->mutex);
+  
   p.t->status = DONE;
-  pthread_mutex_lock (&p.t->current_job->mutex);
-  //access memory in Job
-  p.t->current_job->done++;
-  printf ("Task[%d] done, tasks done until now = %d, total_tasks = %d\n",
-	  p.t->i, p.t->current_job->done, p.t->current_job->size);
-  pthread_mutex_unlock (&p.t->current_job->mutex);
+  
+  pthread_mutex_lock (&p.t->this_pod->mutex);
+  
+  //access memory in Pod
+  p.t->this_pod->done++;
+
+  printf ("Task[%d] done, tasks done until now = %d, total_tasks = %p\n",
+	  p.t->i, p.t->this_pod->done, &p.t->this_pod->size);
+  
+  pthread_mutex_unlock (&p.t->this_pod->mutex);
   return NULL;
 }
 
 void
-sched_jobs (std::vector < Job > j_vec, std::vector < Pod > p_vec)
+sched_pods (std::vector < Pod >& pod_vec, std::vector < Worker >& worker_vec)
   //schedule jobs from the smallest tasks
   //always assign the job that makes more ram
-  //available comparing both Pods
+  //available comparing both Workers
 {
-  int highest_sum = 0;
-  Task *smallest_task = NULL;
-  for (int i = 0; i < p_vec.size (); i++)
-  {
-    for (int j = 0; j < j_vec.size (); j++)
+  for (int ii = 0; ii < worker_vec.size (); ii++)
+  { 
+    int lowest_ram_cmp = INT_MAX;
+    Task *lowest_ram_task = NULL;
+    for (int jj = 0; jj < pod_vec.size (); jj++)
     {
-      if()
+      Task *tmp = pod_vec[jj].get_smallest_task_ram();
+      if (tmp == NULL) continue;
+      else if(
+          worker_vec[ii].ram - tmp->ram >= 0 && 
+          worker_vec[ii].cpu - tmp->cpu >= 0 &&
+          tmp->ram < lowest_ram_cmp)
+      {
+        lowest_ram_cmp = tmp->ram;
+        lowest_ram_task = tmp;
+      }
+      else
+      {
+        printf("OUT OF MEMORY IN WORKER[%d]\n",ii);
+      }
     }
+    if(lowest_ram_task != NULL)
+      worker_vec[ii].add_worker(lowest_ram_task);
   }
 }
+
+bool 
+are_pods_done(std::vector< Pod >& pod_vec)
+{
+  for(int ii = 0; ii < pod_vec.size(); ii++)
+  {
+    if(pod_vec[ii].size <= pod_vec[ii].done)
+      return false;
+  }
+  return true;
+}
+
+
 
 int
 main ()
 {
   //srand(time(NULL));
-  std::vector < Job > j_vec = {
-    Job (3),
-    Job (2)
+  std::vector < Pod > p_vec = {
+    Pod (3),
+    Pod (2)
   };
 
-  std::vector < Pod > p_vec = {
-    Pod (4000, 16),
-    Pod (3000, 8)
+  std::vector < Worker > w_vec = {
+    Worker (4000, 16),
+    Worker (3000, 8)
   };
 
 #if DEBUG == 1
-  std::cout << "smallest ram = " << j_vec[0].get_smallest_task_ram ()->
+  std::cout << "smallest ram = " << p_vec[0].get_smallest_task_ram ()->
     ram << std::endl;
-  std::cout << "biggest ram = " << j_vec[0].get_biggest_task_ram ()->
+  std::cout << "biggest ram = " << p_vec[0].get_biggest_task_ram ()->
     ram << std::endl;
-  std::cout << "smallest cpu = " << j_vec[0].get_smallest_task_cpu ()->
+  std::cout << "smallest cpu = " << p_vec[0].get_smallest_task_cpu ()->
     cpu << std::endl;
-  std::cout << "biggest cpu = " << j_vec[0].get_biggest_task_cpu ()->
+  std::cout << "biggest cpu = " << p_vec[0].get_biggest_task_cpu ()->
     cpu << std::endl;
 #endif
-  while (1)
+
+  while (are_pods_done(p_vec) != false)
     {
+      for (int i = 0; i < w_vec.size (); i++)
+      {
+          printf ("w_vec[%d] = .ram=%d, .cpu=%d\n",
+            i, w_vec[i].ram, w_vec[i].cpu);
+      }
+
       for (int i = 0; i < p_vec.size (); i++)
       {
-          printf ("p_vec[%d] = .ram=%d, .cpu=%d\n",
-          i, p_vec[i].ram, p_vec[i].cpu);
+          printf ("p_vec[%d] = .size=%d, .done=%d\n",
+            i, p_vec[i].size, p_vec[i].done);
       }
-      sleep (1);
+
+      sched_pods(p_vec,w_vec);
+      sleep(1);
     };
 
   return 0;
